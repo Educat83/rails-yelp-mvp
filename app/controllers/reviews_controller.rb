165813d155499fc35f80b0c8c30493145a0db4e9
Eduardo Guerrero <eduardo.guerrero.lara@gmail.com>
2023-05-24 16:53:15 +0100

    Destroy option completed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,22 @@
 class ReviewsController < ApplicationController
   before_action :set_restaurant, only: %i[new create]
 
+  def show
+    @review = Review.find(set_restaurant)
+  end
+
+  def new
+    @review = Review.new
+  end
+
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants do
-    resources :reviews, only: [:create, :show]
+    resources :reviews, only: [:create, :new]
   end
-  resources :reviews, only: [:destroy]
+  resources :reviews, only: [:show, :edit, :update, :destroy]
 end
